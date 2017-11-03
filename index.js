4120db132e589511b721d1e0c5aba3cd218a95e0
coolguythatscool <grgvoicu@gmail.com>
2017-11-03 16:56:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 const Cleverbot = require("cleverbot-node");
 const anti_spam = require("discord-anti-spam");
+const dateFormat = require('dateformat');
 const clbot = new Cleverbot;
 const bot = new Discord.Client();
 
@@ -11,6 +12,7 @@ const O = process.env.ownerid;
 const T = process.env.token;
 const I = process.env.invite;
 
+dateFormat('mmmm dS, dddd, yyyy, h:MM:ss TT');
 clbot.configure({botapi: CL});
 
 var fortunes = [
@@ -486,6 +488,80 @@ bot.on("message", async function(message){
             const setStatus = paramsstat.join(" ");
             bot.setGame(setStatus)
             message.channel.send(`Set the bots game status to: ${setStatus}`)
+            break;
+
+        
+        case "userinfo":
+            if (!msg.guild) {
+                throw 'This can only be used in a guild!';
+            }
+    
+            //Makes sure user mentions a user to get info for
+            if (msg.mentions.users.size < 1) {
+                throw '@mention someone to find the info';
+            }
+    
+            let user = msg.mentions.users.first();
+            let member = msg.guild.member(user);
+    
+            if (!member) {
+                throw 'That member could not be found!';
+            }
+    
+            //How long ago the account was created
+            const millisCreated = new Date().getTime() - user.createdAt.getTime();
+            const daysCreated = millisCreated / 1000 / 60 / 60 / 24;
+    
+            //How long about the user joined the server
+            const millisJoined = new Date().getTime() - member.joinedAt.getTime();
+            const daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+    
+            // Slice off the first item (the @everyone)
+            let roles = member.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => role.name);
+            if (roles.length < 1) roles = ['None'];
+    
+            let embed = bot.utils.embed(
+                `${user.username}#${msg.mentions.users.first().discriminator}`,
+                '***This message will dissappear in 60 seconds.***',
+                [
+                    {
+                        name: 'Status',
+                        value: `${user.presence.status[0].toUpperCase() + user.presence.status.slice(1)}`,
+                    },
+                    {
+                        name: 'Game',
+                        value: `${(user.presence.game && user.presence.game && user.presence.game.name) || 'Not playing a game.'}`,
+                    },
+                    {
+                        name: 'Created On',
+                        value: `${dateFormat(user.createdAt)}`,
+                    },
+                    {
+                        name: 'Days Since Creation',
+                        value: `${daysCreated.toFixed(0)}`,
+                    },
+                    {
+                        name: 'Joined On',
+                        value: `${dateFormat(member.joinedAt)}`,
+                    },
+                    {
+                        name: 'Days Since Joining',
+                        value: `${daysJoined.toFixed(0)}`,
+                    },
+                    {
+                        name: 'Roles',
+                        value: `${roles.join(', ')}`,
+                        inline: false,
+                    },
+                ],
+                {
+                    inline: true,
+                    footer: `User ID: ${user.id}`,
+                    thumbnail: user.displayAvatarURL
+                }
+            );
+    
+            (await msg.edit({ embed })).delete(60000);
             break;
 
         default:
